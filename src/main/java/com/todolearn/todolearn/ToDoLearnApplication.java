5f20481a42de6f0652299e5ee925d4a31eb90703
Monudhakad1 <monudhakad055@gmail.com>
2025-07-05 11:41:02 +0530

    Starting of the Project

---
+++ src/main/java/com/todolearn/todolearn/ToDoLearnApplication.java
@@ -0,0 +1,13 @@
+package com.todolearn.todolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todolearn/todolearn/ToDoLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolearn.todolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
