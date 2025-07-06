498e8f9ff273405c0c7df2b80be48ddab3f43631
Monudhakad1 <monudhakad055@gmail.com>
2025-07-06 21:23:47 +0530

    Thymeleaf config and add controller of add all

--- src/main/java/com/todolearn/todolearn/Services/TodoService.java
+++ src/main/java/com/todolearn/todolearn/Services/TodoService.java
@@ -1,8 +1,22 @@
 package com.todolearn.todolearn.Services;
 
+import com.todolearn.todolearn.models.Task;
+import com.todolearn.todolearn.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
+
 public class TodoService {
+    private final TaskRepository taskRepository;
+    public TodoService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
 }
--- src/main/java/com/todolearn/todolearn/controllers/TaskController.java
+++ src/main/java/com/todolearn/todolearn/controllers/TaskController.java
@@ -1,17 +1,26 @@
 package com.todolearn.todolearn.controllers;
 
 import com.todolearn.todolearn.Services.TodoService;
+import com.todolearn.todolearn.models.Task;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TodoService todoService;
     public TaskController(TodoService todoService) {
         this.todoService = todoService;
     }
-
-    public String getTask(){
-
+    @GetMapping
+    public String getTask(Model model){
+        List<Task> tasks =todoService.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return "tasks";
     }
 }
